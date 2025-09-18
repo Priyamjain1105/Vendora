13a1b0a653a20ce8c394b965dc68fe72b0504b0f
[Priyamjain1105] <[Priyamjainofficial@gmail.com]>
2025-09-18 21:47:59 +0530

    added new changes

--- server/app.py
+++ server/app.py
@@ -8,7 +8,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__,template_folder = 'templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:arunrockstar@localhost/vendor'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:arunrockstar@localhost/vendor'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://avnadmin:AVNS_baarj8CFS79MJev_YIC@mysql-3b0838a6-priyamjainsocial-b642.i.aivencloud.com:27509/defaultdb'
     #basedir = os.path.abspath(os.path.dirname(__file__))
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database', 'vendor.db') # This will create a file named mylife.db in your project directory
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress a warning
