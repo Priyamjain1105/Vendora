b8876d898b632947c8d8160b73b5fbb5baedba43
[Priyamjain1105] <[Priyamjainofficial@gmail.com]>
2025-09-18 21:56:40 +0530

    added new changes

--- server/app.py
+++ server/app.py
@@ -3,13 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sqlite3
 import os
+from dotenv import load_dotenv
+load_dotenv()
 #database object
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__,template_folder = 'templates')
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:arunrockstar@localhost/vendor'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://avnadmin:AVNS_baarj8CFS79MJev_YIC@mysql-3b0838a6-priyamjainsocial-b642.i.aivencloud.com:27509/defaultdb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://avnadmin:{os.getenv("DB_PASSWORD")}@mysql-3b0838a6-priyamjainsocial-b642.i.aivencloud.com:27509/defaultdb'
     #basedir = os.path.abspath(os.path.dirname(__file__))
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database', 'vendor.db') # This will create a file named mylife.db in your project directory
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress a warning
